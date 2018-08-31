0a516934acd6d5f67fd652b90914567da51501d7
Arthur <arthur.jmacedo@gmail.com>
2018-08-31 16:56:34 -0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  url = 'https://mp.s81c.com/8034F2C/dal05/v1/AUTH_db1cfc7b-a055-460b-9274-1fd3f11fe689/675ec0fa49ed9ea3d6fb171e6e58abbb/hero_7a3c416e-f8b1-44d8-aae0-b2706a9248ee.png';
+  description = 'Server IBM';
+}
