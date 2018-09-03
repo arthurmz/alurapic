5afe743689f1ca02ca35a09c278f26f61e0cf924
Arthur <arthur.jmacedo@gmail.com>
2018-09-03 17:19:32 -0300

    Componentes e diretiva ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  url = 'https://mp.s81c.com/8034F2C/dal05/v1/AUTH_db1cfc7b-a055-460b-9274-1fd3f11fe689/675ec0fa49ed9ea3d6fb171e6e58abbb/hero_7a3c416e-f8b1-44d8-aae0-b2706a9248ee.png';
-  description = 'Server IBM';
+
+
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    }
+  ];
 }
