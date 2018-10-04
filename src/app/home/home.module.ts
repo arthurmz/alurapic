8b28dbffefd77e1b31dea482f7ee9d96f4545907
Arthur <arthur.jmacedo@gmail.com>
2018-10-04 16:34:07 -0300

    Fim do cap 1 parte 2.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { compileComponentFromMetadata } from '@angular/compiler';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+    declarations: [ SignInComponent ],
+    imports: [ 
+        ReactiveFormsModule,
+        CommonModule,
+        VMessageModule,
+        RouterModule
+    ]
+})
+export class HomeModule {
+
+}
